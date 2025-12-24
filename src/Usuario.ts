f7c3f9ba80152ab6160c1a5be6a9657a3e4f3f58
renn1805 <renan.almeida.arau@gmail.com>
2025-12-24 12:34:32 -0300

    Remoção de métodos internos da classe usuarios que são despensaveis até agora, atualização da escrita dos atributos do usuarios para um sintaxe simplificada de private e utilização do método "toJSON".

--- src/Usuario.ts
+++ src/Usuario.ts
@@ -1,43 +1,40 @@
 
 export default class Usuario {
-    nomeUsuario: String;
-    private _emailUsuario: String;
-    get emailUsuario (){
-        return this._emailUsuario
+    #nomeUsuario: string;
+    get nomeUsuario() {
+        return this.#nomeUsuario
     }
-    private senhaUsuario: String;
-    profissaoUsuario?: String | undefined;
 
-    constructor(nome: String, email: String, senha:String, profissao?: String){
-        this.nomeUsuario = nome;
-        this._emailUsuario = email;
-        this.senhaUsuario = senha;
-        this.profissaoUsuario = profissao
+    #emailUsuario: string;
+    get emailUsuario() {
+        return this.#emailUsuario
+    }
+
+    #senhaUsuario: string;
+
+    #profissaoUsuario?: string | undefined;
+    get profissaoUsuario() {
+        return this.#profissaoUsuario
+    }
+
+    constructor(nome: string, email: string, senha: string, profissao?: string) {
+        this.#nomeUsuario = nome.toLowerCase();
+        this.#emailUsuario = email.toLowerCase();
+        this.#senhaUsuario = senha;
+        this.#profissaoUsuario = profissao
     };
 
-
-    trocarSenhaUsuario (novaSenha: String, confirmarSenha: String): String{
-        const senhasCorrespondem = novaSenha === confirmarSenha
-        if (senhasCorrespondem){
-            this.senhaUsuario = novaSenha
-            return "Senha alterada com sucesso!"
-        }else{
-            return "Senhas não correspondem"
-        }
+    compararSenha(senhaComparacao: string): Boolean {
+        if (this.#senhaUsuario === senhaComparacao) { return true } else { return false }
     }
 
-    trocarEmailUsuario (novoEmail: String, confirmarEmail: String): String{
-        const emailsCorrespondem = novoEmail === confirmarEmail
-        if (emailsCorrespondem){
-            this._emailUsuario = novoEmail
-            return "E-mail alterado com sucesso"
-        }else{
-            return "E-mails não correspondem"
-        }
-    }
 
-    compararSenha(senhaComparacao:String):Boolean{
-        if (this.senhaUsuario === senhaComparacao) {return true}else {return false}
+    toJSON(){
+        return {
+            nome: this.#nomeUsuario,
+            email: this.#emailUsuario,
+            profissao: this.profissaoUsuario != undefined? this.#profissaoUsuario : "Sem profissão registrada" 
+        }
     }
 
 }
