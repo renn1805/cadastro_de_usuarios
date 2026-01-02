c62fded6fbe253ffdaf066d20e47f3700146fdcf
renn1805 <renan.almeida.arau@gmail.com>
2026-01-02 15:45:22 -0300

    Criação dos arquivos do prisma e prismaclient e inicialização do prisma com sqlite

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import express from "express"
 import cors from "cors"
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+// import { PrismaClient } from './generated/prisma';
+
+const adapter = new PrismaBetterSqlite3({
+    url: "file:./prisma/dev.db"
+}, {
+    timestampFormat: 'unixepoch-ms'
+})
+
+// const prisma = new PrismaClient({ adapter })
 
 const app = express()
 
